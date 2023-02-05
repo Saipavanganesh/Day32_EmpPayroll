1908d955eccb7351d54a915fb9b09b22de6140e3
Saipavanganesh <pavanganeshms@gmail.com>
2023-02-05 06:37:01 +0530

    Insertion Query

---
+++ Uc3_InsertQuery.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee_payroll (name, salary, start_date)
+VALUES
+('Ganesh', 30000, '2022-02-01'),
+('Sai', 20000, '2022-02-02'),
+('Pavan', 29000, '2022-02-03'),
+('Raj', 32000, '2022-02-04');
